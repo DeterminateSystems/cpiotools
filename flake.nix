37d8968f190e1008d6853ef5bb44e501c96655ef
Graham Christensen <graham@grahamc.com>
2023-10-10 16:55:51 -0400

    Mechanical migration to FlakeHub

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "cpiotools";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.533189.tar.gz";
 
   outputs =
     { self
